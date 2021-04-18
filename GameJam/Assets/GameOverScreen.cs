87c0a18f4e05579265025cbec3d5a28fbea46570
Santiago Torres Alvarez <55812138+Chewbacaguy@users.noreply.github.com>
2021-04-18 01:02:45 -0500

    GameOver Screen
    Esta en una escena pero solo es de copiar las cosas en la main scene :)

---
+++ GameJam/Assets/GameOverScreen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverScreen : MonoBehaviour
+{
+    public Text nightsText;
+
+    public void Setup(int nights)
+    {
+        gameObject.SetActive(true);
+        nightsText.text = "You survived " +  nights.ToString() + " nights...";
+    }
+
+    public void RestartButton()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ExitButton()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
