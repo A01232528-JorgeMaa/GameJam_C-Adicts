989ead89c60f3ac37f18d7f850734bea0d6a3506
TimeMachin <66319561+TimeMachin@users.noreply.github.com>
2021-04-17 23:07:01 -0500

    Scripts de la linterna y la interaccion con las baterias

---
+++ GameJam/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController cc;
+    public float Velocidad = 12;
+
+    public float Gravedad = -9.81f;
+    public Vector3 velocity;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask floorMask;
+    bool isGrounded;
+
+
+
+
+    public Camera camara;
+    RaycastHit hit;
+    public float distancia = 10f;
+    public GameObject Linterna;
+
+    void Inspection()
+    {
+        bool rayDidHit = Physics.Raycast(
+            camara.transform.position, //Punto de origen
+            camara.transform.forward, //Direccion de disparo
+            out hit, // Variable donde se almacena del objeto colisionado
+            distancia // Distancia del rayo
+            );
+        if (rayDidHit)
+        {
+            string tagObj = hit.transform.tag;
+            if(tagObj == "Power")
+            {
+                TorchBehavior linterna = Linterna.GetComponent<TorchBehavior>();
+                linterna.BatteryRefill();
+                BatteryBehavior Bateria = hit.transform.GetComponent<BatteryBehavior>();
+                Bateria.Taken();
+                Debug.Log("Bateria recargada");
+            }
+        }
+    }
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, floorMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(3 * -2 * Gravedad);
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        cc.Move(move * Velocidad * Time.deltaTime);
+
+
+        velocity.y += Gravedad * Time.deltaTime;
+        cc.Move(velocity * Time.deltaTime);
+        if (Input.GetKeyDown("e"))
+        {
+            Inspection();
+        }
+    }
+}
---
+++ GameJam/Assets/Scripts/TorchBehavior.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchBehavior : MonoBehaviour
+{
+    public Light Spot;
+    public float battery = 10f;
+    public int blinkStart = 4;
+
+    public float parpadeo = .07f;
+    public int randomizer = 0;
+    bool blinking;
+    bool on;
+
+    public void BatteryRefill()
+    {
+        battery += 3;
+    }
+
+    IEnumerator Blink()
+    {
+        while (true)
+        {
+            if (randomizer == 0)
+            {
+                Spot.enabled = false;
+            }
+            else
+            {
+                Spot.enabled = true;
+            }
+            randomizer = Random.Range(0, 15);
+            yield return new WaitForSeconds(parpadeo);
+        }
+    }
+
+    private void Start()
+    {
+        if (battery > 0)
+        {
+            on = Spot.enabled = true;
+            blinking = false;
+        }
+        else
+        {
+            on = blinking = Spot.enabled = false;
+        }
+    }
+
+    void Update()
+    {
+        // Verificacion de bateria sin estado
+        if(battery <= 0)
+        {
+            Spot.enabled = false;
+            battery = 0;
+            StopAllCoroutines();
+        }
+
+        // Verificacion de bateria con estado
+        if(battery > 0 && on)
+        {
+            Spot.enabled = true;
+        }
+
+        // Parpadeo de la linterna
+        if (battery <= blinkStart && battery > 0 && on)
+        {
+            StartCoroutine(Blink());
+            blinking = true;
+        }
+
+        // Encendido y apagado de la linterna
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (on || blinking)
+            {
+                if (blinking)
+                {
+                    StopAllCoroutines();
+                    blinking = false;
+                }
+                Spot.enabled = false;
+                on = false;
+            }
+            else
+            {
+                Spot.enabled = true;
+                on = true;
+                Debug.Log("Linterna encendida con " + battery + " unidades de bateria");
+            }
+        }
+
+        // Consumo de bateria
+        if(battery > 0 && on)
+        {
+            battery -= Time.deltaTime / 4;
+        }
+
+
+        // Aumento de bateria
+        if(Input.GetKeyDown("r"))
+        {
+            BatteryRefill();
+        }
+
+        //Debug.Log("Bateria: " + battery);
+    }
+}
